0a7582478b5759952d4d7436fbc7e63f662d1571
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-18 17:00:14 -0300

    adicionando configuração de env

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -3,7 +3,7 @@ sudo chown -R www-data /home/harmonize/storage
 sudo chown -R www-data /home/harmonize/bootstrap/cache
 cd /home/harmonize/
 sudo composer install
-cp .env.exemple .env
+cp .env.example .env
 php artisan key:generate
 sudo npm install
 sudo npm run prod
